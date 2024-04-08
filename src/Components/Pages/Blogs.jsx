a6f8ed8c91eaa88fc0ab2b3f4f5bde10fabf3053
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-04-08 19:38:34 +0600

    Blogs section updated

--- src/Components/Pages/Blogs.jsx
+++ src/Components/Pages/Blogs.jsx
@@ -23,7 +23,7 @@ const Blogs = () => {
     <div className="mx-8 md:mx-16 my-12">
       <Helmet>
     <title>BloodBay || Blogs</title>
-  </Helmet>;
+  </Helmet>
       <h2 className="text-3xl md:text-6xl mb-3 lato-bold-700 text-gray-800">
         Read our latest blogs
       </h2>
@@ -35,7 +35,7 @@ const Blogs = () => {
       <section className="grid grid-cols-1 md:grid-cols-3 gap-10 my-8 md:my-10">
         {blogs?.map((blog) => (
           <div key={blog?.id}>
-            <div className="border">
+            <div className="border rounded-md shadow-sm">
               <img src={blog?.image} alt="" className="w-full h-[203.98px]" />
               <div className="px-3 pb-4">
                 <h1 className="text-2xl text-gray-800 lato-bold-600 my-3">
