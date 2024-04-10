e4bde8a6be726a500f9fd2015a89f1ec15087625
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-04-10 11:35:30 +0600

    Dashboard Side bar updated

--- src/Components/Dashboard/DashSideBar.jsx
+++ src/Components/Dashboard/DashSideBar.jsx
@@ -1,23 +1,32 @@
-import { BiPlusCircle, BiUser } from "react-icons/bi";
-import { MdDashboard } from "react-icons/md";
+import { BiHealth, BiPlus, BiPlusCircle, BiUser } from "react-icons/bi";
+import { CiMedicalCase } from "react-icons/ci";
+import { MdCreate, MdDashboard } from "react-icons/md";
 import { NavLink } from "react-router-dom";
 
 
 const DashSideBar = () => {
     const dashLinks = (
         <>
+          <NavLink to={"/dash/createReq"}>
+            <button className="text-xl w-full py-2 px-6 mb-2 bg-[#FF0563] text-white hover:bg-[#ff76aa] flex gap-3 items-center">
+             <BiPlus className="text-2xl text-white"></BiPlus> Create Request
+            </button>
+          </NavLink>{" "}
           <NavLink to={"/dash"}>
-            <button className="text-xl w-full py-2 px-6 hover:bg-[#c8cdd5] flex gap-3 items-center">
+            <button className="text-xl w-full py-2 px-6 mb-2 hover:bg-[#c8cdd5] flex gap-3 items-center">
              <MdDashboard className="text-2xl text-black"></MdDashboard> Dashboard
             </button>
           </NavLink>{" "}
-          <br />
-          <NavLink to={"/dash"}>
-          <button className="text-xl w-full py-2 px-6  hover:bg-[#c8cdd5] flex gap-3 items-center">
+          <NavLink to={"/dash/profile"}>
+          <button className="text-xl w-full py-2 px-6 mb-2 hover:bg-[#c8cdd5] flex gap-3 items-center">
              <BiUser className="text-2xl text-black"></BiUser> Profile
             </button>
           </NavLink>{" "}
-          <br />
+          <NavLink to={"/dash/myReq"}>
+          <button className="text-xl w-full py-2 px-6 mb-2 hover:bg-[#c8cdd5] flex gap-3 items-center">
+             <CiMedicalCase className="text-2xl text-black"></CiMedicalCase> My Request
+            </button>
+          </NavLink>{" "}
         </>
       );
     return (
