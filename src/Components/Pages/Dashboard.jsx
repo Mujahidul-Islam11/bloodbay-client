e000e0ca206504b0e724f040bc0fabe0901f04ce
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-06-20 23:28:50 +0600

    Created a responsive dashboard side bar for small device

--- src/Components/Pages/Dashboard.jsx
+++ src/Components/Pages/Dashboard.jsx
@@ -7,9 +7,17 @@ import { useState } from "react";
 const Dashboard = () => {
     const [drawer, setDrawer] = useState(false);
   return (
-    <div>
+    <div className="relative">
+       {drawer && <div className="absolute z-50 h-screen flex lg:hidden justify-between">
+       <div onClick={()=> setDrawer(false)} className="shadow-lg">
+       <DashSideBar></DashSideBar>
+       </div>
+       <div onClick={()=>setDrawer(false)} className="text-4xl -ml-12 mt-2">
+       <ion-icon name="close-circle-outline"></ion-icon>
+       </div>
+       </div>}
       <header className="block lg:hidden flex items-center gap-10 bg-gradient-to-r from-[#FF0563] to-[#ff05659a] py-3 px-5 text-3xl font-semibold text-white">
-      <div className="">
+      <div onClick={()=> setDrawer(true)} className="">
       <ion-icon name="menu-outline"></ion-icon>
       </div>
         <h3 className="text-center text-xl md:text-3xl">
