766cb983ede9a2baea3258261fb8df342bcb77d7
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-06-22 12:39:00 +0600

    Created profile information for profile component

--- src/Components/Dashboard/Profile.jsx
+++ src/Components/Dashboard/Profile.jsx
@@ -1,11 +1,47 @@
-
+import { useContext } from "react";
+import { AuthContext } from "../../AuthProvider";
 
 const Profile = () => {
-    return (
-        <div>
-            <h3>Hello brofdfdfdfdfdfdfd</h3>
-        </div>
-    );
+  const { user } = useContext(AuthContext);
+  return (
+    <div className="py-6 flex justify-center items-center h-screen">
+      <main className="flex justify-between">
+        <section>
+          <img
+            src={user?.photoURL}
+            className="size-32 object-cover rounded-full flex justify-center mx-auto"
+            alt=""
+          />
+          <h3 className="text-4xl font-semibold my-6 text-center">
+            {user?.displayName}
+          </h3>
+          <div className="flex gap-12">
+            <div>
+              <h3 className="text-xl mb-2">
+                <b>Email:</b> {user?.email}
+              </h3>
+              <h3 className="text-xl mb-2">
+                <b>Role:</b> Donor
+              </h3>
+              <h3 className="text-xl mb-2">
+                <b>Upazila:</b> Rangunia
+              </h3>
+            </div>
+            <div>
+              <h3 className="text-xl mb-2">
+                <b>Blood Group:</b> O+
+              </h3>
+              <h3 className="text-xl mb-2">
+                <b>District:</b> Chattogram
+              </h3>
+            </div>
+          </div>
+        </section>
+        <section>
+        </section>
+      </main>
+    </div>
+  );
 };
 
 export default Profile;
--- src/Components/Pages/Dashboard.jsx
+++ src/Components/Pages/Dashboard.jsx
@@ -1,7 +1,5 @@
 import { Outlet } from "react-router-dom";
-import DashHome from "../Dashboard/DashHome";
 import DashSideBar from "../Dashboard/DashSideBar";
-import { FaHamburger } from "react-icons/fa";
 import { useState } from "react";
 
 const Dashboard = () => {
