4e135f0e59363f91355b20513c449838a2870ad7
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-06-26 21:03:57 +0600

    Added a static donation request table for donation request page

--- src/Components/Pages/DonationReq.jsx
+++ src/Components/Pages/DonationReq.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { AuthContext } from "../../AuthProvider";
 import { Helmet } from "react-helmet";
+import { NavLink } from "react-router-dom";
 
 const DonationReq = () => {
   const { user } = useContext(AuthContext);
@@ -15,8 +16,61 @@ const DonationReq = () => {
           Hello, <span className="text-[#FF0563]">{user?.displayName}</span>
         </h1>
       </header>
-      <main className="">
-
+      <main className="my-14 px-6">
+        <div className="overflow-x-auto border rounded-md">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>Recipient Name</th>
+                <th>Recipient Location</th>
+                <th>Donation Date</th>
+                <th>Donation Time</th>
+                <th>Donation Status</th>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              <tr>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div>
+                      <div className="font-bold">Kazi Newaz</div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  <h3>Rangunia, Chattogram</h3>
+                </td>
+                <td>
+                    6/22/2024
+                </td>
+                <td>
+                    6:00 PM
+                </td>
+                <td>
+                  <h2 className="bg-green-600 text-white text-center py-2">
+                    In-Progress
+                  </h2>
+                </td>
+                <th>
+                  <button className="btn btn-sm bg-blue-600 text-white hover:bg-blue-400">Edit</button>
+                </th>
+                <th>
+                  <button className="btn btn-sm bg-red-600 text-white hover:bg-red-400">Remove</button>
+                </th>
+                <th>
+                  <button className="btn btn-sm bg-green-600 text-white hover:bg-green-400">View</button>
+                </th>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <NavLink to={"/dashboard/myReq"}>
+            <button className="btn btn-md flex justify-center mx-auto my-8 px-6 bg-[#FF0563] text-white  hover:bg-[#ff5174]">View All Requests</button>
+        </NavLink>
       </main>
       </section>
     </div>
