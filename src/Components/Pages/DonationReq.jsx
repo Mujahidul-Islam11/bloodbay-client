de9c8b4c4478a6420c29c8b50efdcb3bcd00bbd3
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-06-26 20:44:32 +0600

    Updated the donation request page

--- src/Components/Pages/DonationReq.jsx
+++ src/Components/Pages/DonationReq.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { AuthContext } from "../../AuthProvider";
-import {Helmet} from "react-helmet"
+import { Helmet } from "react-helmet";
 
 const DonationReq = () => {
   const { user } = useContext(AuthContext);
@@ -9,9 +9,11 @@ const DonationReq = () => {
       <Helmet>
         <title>BloodBay || Donation Request</title>
       </Helmet>
-      <h1 className="text-4xl text-center text-gray-800 lato-bold-700">
-        Hello, <span className="text-[#FF0563]">{user?.displayName}</span>
-      </h1>
+      <header>
+        <h1 className="text-3xl text-center text-gray-800 lato-bold-700">
+          Hello, <span className="text-[#FF0563]">{user?.displayName}</span>
+        </h1>
+      </header>
     </div>
   );
 };
