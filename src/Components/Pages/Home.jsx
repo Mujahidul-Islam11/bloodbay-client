2195ceeae758578ad9004010b97651b2f4ac9da9
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-06-30 11:20:28 +0600

    Imported how it works component inside the home component

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -2,6 +2,7 @@ import Contact_US from "../Contact_US";
 import Banner from "./Banner";
 import Testimonials from "../Testimonials";
 import { Helmet } from "react-helmet";
+import How_It_Works from "../How_It_Works";
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
         <title>BloodBay || Donate Blood Easily</title>
       </Helmet>
       <Banner></Banner>
+      <How_It_Works></How_It_Works>
       <Contact_US></Contact_US>
       <Testimonials></Testimonials>
     </div>
