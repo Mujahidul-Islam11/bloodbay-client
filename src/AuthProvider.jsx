7de30e9e7a7ace278f25853aed688efd6b12ed42
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-07-13 20:03:25 +0600

    Profile updating component title created

--- src/AuthProvider.jsx
+++ src/AuthProvider.jsx
@@ -14,18 +14,25 @@ const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
+  // register
   const createUser = (email, pass) => {
     setLoading(true);
     return createUserWithEmailAndPassword(auth, email, pass);
   };
+
+  // login
   const logIn = (email, pass) => {
     setLoading(true);
     return signInWithEmailAndPassword(auth, email, pass);
   };
+
+  // logout
   const logOut = () => {
     setLoading(true);
     return signOut(auth);
   };
+
+  // updateProfile while creating user
   const upProfile = (name, imageUrl) => {
     setLoading(true);
     return updateProfile(auth.currentUser, {
@@ -33,6 +40,7 @@ const AuthProvider = ({ children }) => {
       photoURL: imageUrl,
     });
   };
+  
   useEffect(()=>{
     const unSubscribe = onAuthStateChanged(auth, (currentUser)=>{
         setUser(currentUser);
--- src/Components/Dashboard/ProfileEdit.jsx
+++ src/Components/Dashboard/ProfileEdit.jsx
@@ -1,11 +1,13 @@
-
-
 const ProfileEdit = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="py-6 flex flex-col justify-center items-center lg:h-screen">
+      <header>
+        <h1 className="text-3xl text-center text-gray-800 lato-bold-700">
+         Update your <span className="text-[#FF0563]">Profile!</span>
+        </h1>
+      </header>
+    </div>
+  );
 };
 
 export default ProfileEdit;
